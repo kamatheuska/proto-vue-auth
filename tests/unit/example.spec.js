811ce35cbe0f0bd84bc3517b5642e8bc6e14901f
Nicolas Ramirez <nico9017@gmail.com>
2018-10-22 19:17:26 +0200

    Boilerplate for components ready

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Login from '@/components/Login.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
+describe('Login.vue', () => {
+    it('renders props.msg when passed', () => {
+        const msg = 'new message'
+        const wrapper = shallowMount(Login, {
+            propsData: { msg }
+        })
+        expect(wrapper.text()).toMatch(msg)
     })
-    expect(wrapper.text()).toMatch(msg)
-  })
 })
